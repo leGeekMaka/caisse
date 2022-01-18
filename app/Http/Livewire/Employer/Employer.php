453e8205f1537f2abf589b1fa247b57d793907b9
leGeekMaka <alexgobe92@gmail.com>
2022-01-18 15:57:48 +0100

    gestion des erreurs + modifier et supprimer une demande

--- app/Http/Livewire/Employer/Employer.php
+++ app/Http/Livewire/Employer/Employer.php
@@ -3,13 +3,14 @@
 namespace App\Http\Livewire\Employer;
 
 use App\Models\Employer as ModelsEmployer;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
 class Employer extends Component
 {
     use WithFileUploads;
-    public $object, $message, $status, $path, $fileName;
+    public $object, $message, $status, $path, $fileName,$employeId,$edit = "false";
     const STATUS_1 = "En attente", STATUS_2 = "Accepté";
     public function render()
     {
@@ -18,7 +19,7 @@ class Employer extends Component
         ]);
     }
 
-
+    protected $listeners = ['destroy'];
 
     public $rules = [
         'object' => 'required',
@@ -26,27 +27,94 @@ class Employer extends Component
     ];
     public function store(){
 
-      //dd($this->path->getClientOriginalName());
-      //$name = explode('.', $this->path->getClientOriginalName())[0];
-      //dd($name);
-      //$store = $this->path->store('attach');
-      //dd($store);
         $this->validate();
-        ModelsEmployer::create([
-            'object' => $this->object,
-            'message' => $this->message,
-            'path' => $this->path ? $this->path->store('attach') : '',
-            'fileName' => $this->path ? explode('.', $this->path->getClientOriginalName())[0] : '', // explode renvoi un tableau et j'affiche la premier élement du tableau
-            'status' => Self::STATUS_1,
-        ]);
+        try{
+            ModelsEmployer::create([
+                'object' => $this->object,
+                'message' => $this->message,
+                'path' => $this->path ? $this->path->store('attach') : '',
+                'fileName' => $this->path ? explode('.', $this->path->getClientOriginalName())[0] : '', // explode renvoi un tableau et j'affiche la premier élement du tableau
+                'status' => Self::STATUS_1,
+            ]);
 
-        $this->object = "";
-        $this->message = "";
+            $this->reset('object','message','path','fileName');
+            $this->emit('closeModal');
+            $this->dispatchBrowserEvent('closeAlert');
+            session()->flash('message', 'Démande enregistreé avec succès.');
 
-        $this->emit('closeModal');
-        $this->emit('fileUploaded');
-        $this->dispatchBrowserEvent('closeAlert');
-        session()->flash('message', 'Démande enregistreé avec succès.');
+        }catch(\Exception $e){
+
+            Log::error(sprintf('%d'.$e->getMessage(), __METHOD__));
+            session()->flash('error', 'oups une erreur est survenue, veuillez actualiser et essayer à nouveau.');
+            $this->emit('closeModal');
+            $this->reset('object','message','path','fileName');
+        }
+    }
+
+    /**
+     * @param $operationId
+     */
+    public function edit($operationId)  {
+        $this->edit = "true";
+        $employe = ModelsEmployer::find($operationId);
+        $this->object = $employe->object;
+        $this->message = $employe->message;
+        $this->fileName = $employe->fileName;
+        $this->employeId = $employe->id;
+    }
+
+    public function update(){
+
+        try{
+            ModelsEmployer::find($this->employeId)->update([
+                'object' => $this->object,
+                'message' => $this->message,
+                'path' => $this->path ? $this->path->store('attach') : '',
+                'fileName' => $this->path ? explode('.', $this->path->getClientOriginalName())[0]: '',
+            ]);
+            $this->edit = "false";
+            $this->emit('closeModal');
+            $this->dispatchBrowserEvent('closeAlert');
+            session()->flash('message', 'Mise à jour reussie.');
+
+        } catch(\Exception $e){
+            Log::error(sprintf('%d'.$e->getMessage(), __METHOD__));
+            session()->flash('error', 'oups une erreur est survenue, veuillez actualiser et essayer à nouveau.');
+            $this->emit('closeModal');
+            $this->reset('object','message','path','fileName');
+        }
+    }
+
+
+    /**
+     * @param $operationId
+     * on recupère l'id de l'operation à supprimer
+     */
+    public function getId($employeId){
+
+        try{
+            $employe = ModelsEmployer::find($employeId);
+            $this->object = $employe->object;
+            $this->employeId = $employe->id;
+        }catch (\Exception $e){
+            Log::error(sprintf('%d'.$e->getMessage(), __METHOD__));
+            session()->flash('error', 'oups une erreur est survenue, veuillez actualiser et essayer à nouveau.');
+            $this->reset('object','employeId');
+            $this->emit('closeModalDestroy');
+        }
+    }
+
+    public function destroy(){
+
+        try{
+            ModelsEmployer::find($this->employeId)->delete();
+            $this->reset('object') ;
+        }catch (\Exception $e){
+            Log::error(sprintf('%d'.$e->getMessage(), __METHOD__));
+            session()->flash('error', 'oups une erreur est survenue, veuillez actualiser et essayer à nouveau.');
+            $this->reset('object','employeId');
+            $this->emit('closeModalDestroy');
+        }
     }
 
     public function cancel(){
--- resources/views/livewire/employer/employer.blade.php
+++ resources/views/livewire/employer/employer.blade.php
@@ -60,10 +60,12 @@
                           </td>
                           <td>
                             <div class="action">
-                              <button class="edit" data-bs-toggle="modal">
+                              <button wire:click="edit({{$employer->id}})" class="edit"
+                                      data-bs-toggle="modal" data-bs-target="#ModalTwo" data-bs-toggle="modal">
                                   <i class="lni lni-pencil"></i>
                               </button>
-                              <button class="text-danger" data-bs-toggle="modal">
+                              <button class="text-danger" wire:click="getId({{$employer->id}})"
+                                      data-bs-toggle="modal" data-bs-target="#ModalTree">
                                   <i class="lni lni-trash-can"></i>
                               </button>
                            </div>
@@ -94,7 +96,7 @@
                                 </button>
                             </div>
                             <div class="modal-body">
-                                <form id="form-upload" wire:submit.prevent="store" enctype="multipart/form-data" >
+
                                 <div class="input-style-1">
                                     <label>Objet</label>
                                     <input type="text" wire:model.defer="object" placeholder="objet" />
@@ -107,19 +109,66 @@
                                 </div>
                                 <div class="input-style-1">
                                     <label>Piece jointe</label>
+                                    @if($edit === "true")
+                                    <p> nom du fichier : {{$fileName}}</p>
+                                    @endif
                                     <input class="form-control" type="file" name="path" wire:model.defer="path" placeholder="piece jointe" />
                                     <div wire:loading wire:target="path">Téléchargement...</div>
                                 </div>
                                 <div class="text-center">
-                                    <button class="main-btn active-btn-outline rounded-md btn-hover"
-                                            type="submit">Enregistrer
-                                    </button>
+                                    @if($edit === "true")
+                                        <button class="main-btn danger-btn-outline rounded-md btn-hover" data-bs-dismiss="modal"
+                                                wire:click="cancel">Annuler
+                                        </button>
+                                        <button class="main-btn active-btn-outline rounded-md btn-hover"
+                                                wire:click="update">Modifier
+                                        </button>
+                                    @else
+                                        <button class="main-btn danger-btn-outline rounded-md btn-hover" data-bs-dismiss="modal"
+                                                wire:click="cancel">Annuler
+                                        </button>
+                                        <button class="main-btn active-btn-outline rounded-md btn-hover"
+                                                wire:click="store">Enregistrer
+                                        </button>
+                                    @endif
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+    {{-- modal de suppression--}}
+
+    <div class="warning-modal">
+        <div  wire:ignore.self class="modal fade" data-bs-backdrop="static" data-bs-keyboard="false" id="ModalTree" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content card-style warning-card text-center">
+                    <div class="modal-header px-0 border-0 d-flex justify-content-end ">
+                        <button class="border-0 bg-transparent h1" wire:click="cancel" data-bs-dismiss="modal">
+                            <i class="lni lni-cross-circle"></i>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="icon text-danger mb-20">
+                            <i class="lni lni-warning"></i>
+                        </div>
+                        <div class="content mb-30">
+                            <h2 class="mb-15">Attention !</h2>
+                            <p class="text-sm text-medium">
+                                Vous allez supprimer : <strong> {{ $object }}</strong>
+                            </p>
+                        </div>
+                        <div class="action d-flex flex-wrap justify-content-center">
+                            <button data-bs-dismiss="modal" wire:click="$emit('destroy')" class="main-btn danger-btn rounded-full btn-hover m-1"
+                            > Supprimer
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @push('scripts')
 <script>
